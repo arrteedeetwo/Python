ce2ff7c62d092dd7b48e7bea7382c224af9d557d
arrteedeetwo <rchltwnsnd@gmail.com>
2016-02-08 20:34:55 -0800

    Update snippet.py

--- snippet.py
+++ snippet.py
@@ -1,2 +1,6 @@
 # print numpy array without scientific notation
 np.array_str(x,  suppress_small=True)
+
+
+# make dummy variables out of a variable with categories (logistic regressoin)
+pd.get_dummies()
