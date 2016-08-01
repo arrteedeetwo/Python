fe08cdcf508afe2bca53b915d1633fa4d9bd580c
arrteedeetwo <rchltwnsnd@gmail.com>
2016-08-01 11:21:56 -0700

    added code for graphics issues

--- snippet.py
+++ snippet.py
@@ -4,3 +4,8 @@ np.array_str(x,  suppress_small=True)
 
 # make dummy variables out of a variable with categories (logistic regressoin)
 pd.get_dummies()
+
+# OSX grapichs/framework workaround for venv
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
